c188d2ee53c2df20e7381bd200fc3d251b60c848
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2015-03-12 02:54:24 +0200

    Add database and data model

---
+++ DniproFuture/Models/DniproFutureModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DniproFuture.Models
+{
+    public class DniproFutureModelRepository 
+    {
+        DniproFuture_siteEntities _dbContext = new DniproFuture_siteEntities();
+
+
+    }
+}

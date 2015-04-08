8f03b3c09025f16664b6909bf1868c627dc95f1e
natashalysakova <natashalysakova@gmail.com>
2015-04-08 17:46:14 +0300

    add regExp check file

---
+++ DniproFuture/Models/StringExtention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace DniproFuture.Models
+{
+    public static class StringExtention
+    {
+        public static string GetClearTitle(this string title)
+        {
+            Regex pattern = new Regex("[.,!?:;-]|[\"]");
+
+            string s = pattern.Replace(title, " ");
+            pattern = new Regex("[(\\s+)]");
+            s = pattern.Replace(s, "-");
+
+            return s;
+        }
+    }
+}

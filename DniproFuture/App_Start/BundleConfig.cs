50100db0f80959450781d54c2125dfe032dd952a
natashalysakova <natashalysakova@gmail.com>
2015-04-09 12:32:58 +0300

    add tinymce editor for admin close #25

--- DniproFuture/App_Start/BundleConfig.cs
+++ DniproFuture/App_Start/BundleConfig.cs
@@ -8,31 +8,25 @@ namespace DniproFuture
         // Дополнительные сведения о Bundling см. по адресу http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.unobtrusive*","~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/animation").Include(
-                         "~/Scripts/cbpAnimatedHeader.js"));
+            bundles.Add(new ScriptBundle("~/bundles/animation").Include("~/Scripts/cbpAnimatedHeader.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/agency").Include(
-            "~/Scripts/agency.js"));
-            bundles.Add(new ScriptBundle("~/agency/classie").Include(
-                        "~/Scripts/classie.js"));
+            bundles.Add(new ScriptBundle("~/bundles/agency").Include("~/Scripts/agency.js"));
+            bundles.Add(new ScriptBundle("~/agency/classie").Include("~/Scripts/classie.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/moment").Include("~/Scripts/moment.min.js", "~/Scripts/moment-with-locales.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/dateTimePicker").Include("~/Scripts/bootstrap-datetimepicker.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
-"~/Scripts/admin.js"));
+            bundles.Add(new ScriptBundle("~/bundles/admin").Include("~/Scripts/admin.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/lightboxJS").Include(
-"~/Scripts/ekko-lightbox.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lightboxJS").Include("~/Scripts/ekko-lightbox.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/tinymce").Include("~/Scripts/tinymce/tinymce.min.js"));
 
 
             
--- DniproFuture/Scripts/admin.js
+++ DniproFuture/Scripts/admin.js
@@ -36,4 +36,14 @@ $(function () {
         locale: 'ru',
         format: 'L'
     });
+
+    tinymce.init({
+        selector: 'textarea',
+        plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+    });
 });

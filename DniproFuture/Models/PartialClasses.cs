1e4c49a62a53e85a978fa1292eb8019df982a4d5
natashalysakova <natashalysakova@gmail.com>
2015-04-13 12:48:48 +0300

    Addd inner classes validation close #50 close #51 close #52 close #53 close #54 close #55

---
+++ DniproFuture/Models/PartialClasses.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DniproFuture.Models
+{
+    [MetadataType(typeof(NeedHelpMetadata))]
+    public partial class NeedHelp : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Sum > NeedSum)
+                yield return new ValidationResult("Cумма не должна быть больше необходимой", new[] { "Sum", "NeedSum" });
+
+            if (Birthday > DateTime.Now)
+                yield return new ValidationResult("Дата рождения не должна быть из будущего", new[] { "Birthday" });
+
+            if (StartDate > FinishDate)
+                yield return new ValidationResult("Дата начала сбора должна быть раньше даты окончания", new[] { "FinishDate", "StartDate" });
+        }
+    }
+
+
+    [MetadataType(typeof(NeedHelpLocalSetMetadata))]
+    public partial class NeedHelpLocalSet
+    {}
+
+
+    [MetadataType(typeof(NewsMetadata))]
+    public partial class News
+    {
+    }
+
+    [MetadataType(typeof(NewsLocalSetMetadata))]
+    public partial class NewsLocalSet
+    { }
+
+
+    [MetadataType(typeof(PartnersLocalSetMetadata))]
+    public partial class PartnersLocalSet
+    { }
+    [MetadataType(typeof(PartnersMetadata))]
+    public partial class Partners
+    {
+    }
+
+    [MetadataType(typeof(ProjectsMetadata))]
+    public partial class Projects : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Sum > NeedSum)
+                yield return new ValidationResult("Cумма не должна быть больше необходимой", new[] { "Sum", "NeedSum" });
+
+            if (StartDate > FinishDate)
+                yield return new ValidationResult("Дата начала сбора должна быть раньше даты окончания", new[] { "FinishDate", "StartDate" });
+        }
+    }
+    [MetadataType(typeof(ProjectsLocalSetMetadata))]
+    public partial class ProjectsLocalSet
+    { }
+
+
+
+}
